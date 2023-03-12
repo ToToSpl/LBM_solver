36378d96a9fca0ecfbd247010b134a86601455e7
Jacek Grzybowski <totospl99@gmail.com>
2023-03-12 19:56:39 +0100

    streaming directions

--- include/lbm_constants.h
+++ include/lbm_constants.h
@@ -35,4 +35,18 @@
         1.f / 216.f,                                                           \
   }
 
+// indecies which needs to be streamed in given direction
+#define LBM_STREAM_X_PLUS                                                      \
+  { 1, 7, 9, 13, 15, 19, 21, 23, 26 }
+#define LBM_STREAM_X_MINUS                                                     \
+  { 2, 8, 10, 14, 16, 20, 22, 24, 25 }
+#define LBM_STREAM_Y_PLUS                                                      \
+  { 3, 7, 11, 14, 17, 19, 21, 24, 25 }
+#define LBM_STREAM_Y_MINUS                                                     \
+  { 4, 8, 12, 13, 18, 20, 22, 23, 26 }
+#define LBM_STREAM_Z_PLUS                                                      \
+  { 5, 9, 11, 16, 18, 19, 22, 23, 25 }
+#define LBM_STREAM_Z_MINUS                                                     \
+  { 6, 10, 12, 15, 17, 20, 21, 24, 26 }
+
 #endif
