d6cc6b913a876d746546dad55604f0ef8408029c
Jacek Grzybowski <totospl99@gmail.com>
2023-04-05 16:02:07 +0200

    fixed grid algorithm

--- src/lbm_helpers.cu
+++ src/lbm_helpers.cu
@@ -1,7 +1,26 @@
 #include "./lbm_helpers.cuh"
 #include "assert.h"
 
-#define INTEGER_DIV_CEIL(X, Y) ((X + (Y + 1)) / Y)
+// Data taken from:
+// https://stackoverflow.com/questions/5062781/cuda-max-threads-in-a-block
+#define MAX_BLOCK_SIZE 1024
+#define MAX_BLOCK_X 1024
+#define MAX_BLOCK_Y 1024
+#define MAX_BLOCK_Z 64
+// Values choosen for best 2d and 3d alignment
+#define DEFAULT_BLOCK_X 32
+#define DEFAULT_BLOCK_Y 32
+#define DEFAULT_BLOCK_Z 1
+
+inline size_t ceil_grid(size_t a, size_t b) {
+  // returns ceil of a / b
+  size_t mod = a / b;
+  size_t rest = a % b;
+  if (rest == 0)
+    return mod;
+  else
+    return mod + 1;
+}
 
 ComputeDim compute_dim_create(size_t x, size_t y, size_t z) {
   assert(x > 0 && y > 0 && z > 0);
@@ -14,10 +33,13 @@ ComputeDim compute_dim_create(size_t x, size_t y, size_t z) {
     return {dim3(1, 1, 1), dim3(x, y, z)};
   }
   // otherwise divide space using default system
-  size_t grid_x = INTEGER_DIV_CEIL(x, DEFAULT_BLOCK_X);
-  size_t grid_y = INTEGER_DIV_CEIL(y, DEFAULT_BLOCK_Y);
-  size_t grid_z = INTEGER_DIV_CEIL(z, DEFAULT_BLOCK_Z);
+  size_t grid_x = ceil_grid(x, DEFAULT_BLOCK_X);
+  size_t grid_y = ceil_grid(y, DEFAULT_BLOCK_Y);
+  size_t grid_z = ceil_grid(z, DEFAULT_BLOCK_Z);
 
-  return {dim3(grid_x, grid_y, grid_z),
-          dim3(DEFAULT_BLOCK_X, DEFAULT_BLOCK_Y, DEFAULT_BLOCK_Z)};
+  size_t block_x = grid_x == 1 ? x : DEFAULT_BLOCK_X;
+  size_t block_y = grid_y == 1 ? y : DEFAULT_BLOCK_Y;
+  size_t block_z = grid_z == 1 ? z : DEFAULT_BLOCK_Z;
+
+  return {dim3(grid_x, grid_y, grid_z), dim3(block_x, block_y, block_z)};
 }
--- src/lbm_helpers.cuh
+++ src/lbm_helpers.cuh
@@ -2,17 +2,6 @@
 #define LBM_HELPERS_INCLUDE
 #include <cuda.h>
 
-// Data taken from:
-// https://stackoverflow.com/questions/5062781/cuda-max-threads-in-a-block
-#define MAX_BLOCK_SIZE 1024
-#define MAX_BLOCK_X 1024
-#define MAX_BLOCK_Y 1024
-#define MAX_BLOCK_Z 64
-// Values choosen for best 2d and 3d alignment
-#define DEFAULT_BLOCK_X 32
-#define DEFAULT_BLOCK_Y 32
-#define DEFAULT_BLOCK_Z 1
-
 typedef struct {
   dim3 gridSize, blockSize;
 } ComputeDim;
